9592b5193b1329fcc535902ac03603bbdc5b15ad
David Chung <david23@my.yorku.ca>
2020-05-30 02:08:55 -0400

    Updates

---
+++ precache-manifest.33e1d8515fd54c8d7df65635dc3ba9b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8403968c86a5117d9e81466d7dd361f1",
+    "url": "/Lemonade-Stand/index.html"
+  },
+  {
+    "revision": "9d9363e4338e6f59b65a",
+    "url": "/Lemonade-Stand/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9d9363e4338e6f59b65a",
+    "url": "/Lemonade-Stand/static/js/2.16677f4e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Lemonade-Stand/static/js/2.16677f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9210cc91e0ffd346a0e",
+    "url": "/Lemonade-Stand/static/js/main.1affe936.chunk.js"
+  },
+  {
+    "revision": "c863ac76c033f444cfcc",
+    "url": "/Lemonade-Stand/static/js/runtime-main.c96ad513.js"
+  },
+  {
+    "revision": "89c6f4dfe533c04d6d24708306257d42",
+    "url": "/Lemonade-Stand/static/media/lemonade.89c6f4df.jpg"
+  }
+]);
